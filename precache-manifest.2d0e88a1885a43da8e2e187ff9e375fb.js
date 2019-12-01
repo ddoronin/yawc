3075681b9b646669a31e540e0bad18a41a63fb02
Dima Doronin <doronindm@gmail.com>
2019-12-01 13:51:13 -0500

    Updates

---
+++ precache-manifest.2d0e88a1885a43da8e2e187ff9e375fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "765f1c1d442cc967cce93ae40fa838eb",
+    "url": "/yawc/index.html"
+  },
+  {
+    "revision": "8932af9428c698f2ec2d",
+    "url": "/yawc/static/css/main.21466e8e.chunk.css"
+  },
+  {
+    "revision": "c2ddb0a66c51e00e48b9",
+    "url": "/yawc/static/js/2.bc266244.chunk.js"
+  },
+  {
+    "revision": "8932af9428c698f2ec2d",
+    "url": "/yawc/static/js/main.2706fd2a.chunk.js"
+  },
+  {
+    "revision": "675ec3842da78b517d1f",
+    "url": "/yawc/static/js/runtime-main.a70a9d68.js"
+  }
+]);
